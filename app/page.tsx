5c5c3b88d6970ad60deafc8b9c097ea7588a01e5
suii2210 <work8758@gmail.com>
2024-12-13 20:55:05 +0530

    Added full code

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import ObjectDetection  from "@/components/object-detection";
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex min-h-screen flex-col items-center p-8">
+    <h1 className=" gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">The detector</h1></div>
+
+    <ObjectDetection/>
+    </>
+  );
+}
